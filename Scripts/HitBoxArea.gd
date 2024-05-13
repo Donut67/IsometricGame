07b16661536cf27a540504e045a687fca028c7a0
Donut <adri2000gm@gmail.com>
2024-05-14 00:02:57 +0200

    reset

--- Scripts/HitBoxArea.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-@onready var player = get_tree().get_nodes_in_group("player")[0]
-var can_attack = false
-
-func _process(delta):
-	can_attack = overlaps_body(player)
--- Scripts/Title Screen.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-
-func _on_Start_Game_pressed():
-	Global._goto_scene("game")
-
-func _on_Exit_pressed():
-	get_tree().quit()
