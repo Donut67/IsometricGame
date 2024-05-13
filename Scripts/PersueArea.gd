169a7e226e5c3e07c648331724c552ebb12947e1
Donut <adri2000gm@gmail.com>
2024-05-13 23:48:22 +0200

    first commit

--- Scripts/PersueArea.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-@onready var player = get_tree().get_nodes_in_group("player")[0]
-var can_persue = false
-
-func _process(delta):
-	can_persue = overlaps_body(player) and get_parent().state == "Idle"
-
